fbc7f1820622287b7df438b215c31884a0ed3b96
jondea <jondea@thedeakin.net>
2016-01-24 11:10:53 +0000

    Finally got conenction to database working, with an example for adding and retreiving docs

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ console.log('hi');
 
 var mongoose = require('mongoose');
  
-mongoose.connect('ds047865.mongolab.com:47865/darren-the-databases');
+mongoose.connect('mongodb://trevor-the-admin:potato@ds047865.mongolab.com:47865/darren-the-database');
 
-var Cat = mongoose.model('user', { 
+var user = mongoose.model('user', { 
   name: String,
   age: Number 
 });
@@ -13,7 +13,17 @@ var localUser = new user({
   name: 'Kevin',
   age: 21
 });
-kitty.save(function (err) {
+
+localUser.save(function (err) {
   if (err) // ...
   console.log('Oh dear, Kevin couldn\'t be created');
+});
+
+
+user.findOne({ name: 'Bob'}, function (err, doc){
+  // doc is a Document
+  if (err) // ...
+  console.log('Oh dear, Kevin couldn\'t be created');
+  else
+  console.log(doc.name);
 });
